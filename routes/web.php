2cc508ed6a8f0a3145bc33a1241ea1b9ee588814
Elton Sidney Pedro de Sousa <sidneymelodia@Eltons-MBP.home>
2024-04-22 11:03:36 +0200

    created routes with view. closure and view method

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    
+    // do something before
+
+    return view("home");
 });
+
+Route::view('/about', 'about');
