8f5c40100e15378f65a0d8b481bc98311d840bf1
SidneySousa <sidney.sousa@mtn.com>
2024-04-22 13:59:30 +0200

    layouts final

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About view</title>
-</head>
-<body>
-    
-    <header>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+@extends('layouts.front')
 
-    <main>
-        <h1>About this website</h1>
-    </main>
-    
-</body>
-</html>
+@section('meta-title', 'About title meta')
+
+@section('main-content')
+
+<div>
+    <h2>
+        About page sub heading
+    </h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum cum veritatis libero? Voluptas saepe laudantium tenetur officia ab non! Corporis facilis, vero adipisci recusandae deleniti earum ratione! Harum, consectetur dicta?</p>
+</div>
+
+@endsection
